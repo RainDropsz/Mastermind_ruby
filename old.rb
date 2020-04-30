ce63bfa9abcbc780f85758ceb40050483a1e9e8f
RainDropsz <raindropsz100@gmail.com>
2020-04-30 02:40:43 -0400

    added computer AI

--- old.rb
+++ old.rb
@@ -31,6 +31,8 @@ class Mastermind
         computer_guesses.misplaced_array[i] = score.misplaced
       end
 
+    
+
       if score.winner?
         puts "Congratulations! You guessed the code!"
         return
@@ -69,73 +71,42 @@ class Mastermind
 
 end
 
-
 class ComputerGuesses
   attr_reader :guess_array
   attr_accessor :correct_array, :misplaced_array
-  def initialize 
-    @valid_guesses = "123456789"    
-    @current_guess = [rand_digit, rand_digit, rand_digit]
-    @guess_array = Array.new(2, @current_guess.join(""))
-    @correct_array = Array.new(13,0)
-    @misplaced_array = Array.new(13,0)    
-    @perfect_spot = "01"
-  end
 
+  def initialize
+    @guess_array = []
+    @correct_array = []
+    @misplaced_array = []    
+    @valid_guesses = "123456789"
+    @weight = [0,1,1,1,1,1,1,1,1,1]
+  end
 
   def guess(i)    
-    if i > 1
-      num_correct = @correct_array[i-1].to_i
-      num_misplaced = @misplaced_array[i-1].to_i
+    num_correct = @correct_array[i-1].to_i
+    num_misplaced = @misplaced_array[i-1].to_i
 
-      if num_correct + num_misplaced == 0 && i > 1
-        @valid_guesses = @valid_guesses.delete(@guess_array[i-1])
-      elsif num_correct + num_misplaced == 3 && i > 1
-        @valid_guesses = @guess_array[i-1]
-      end
-
-      #find index with most perfects, hold # perfect constant
-      index_of_max = @correct_array[1..-1].index(@correct_array[1..-1].max) + 1
-      max_perfect = @correct_array[1..-1].max
-      most_perfect_guess = @guess_array[index_of_max]
-
-      if @correct_array[i-1] != max_perfect
-        @perfect_spot = next_try
-      end
-
-      if @misplaced_array[i-1] > 1
-        @valid_guesses << guess_array[i-1]
-      end
-
-      while true   
-        if max_perfect == 1
-          @current_guess = [rand_digit, rand_digit, rand_digit]      
-          @current_guess[@perfect_spot[0].to_i] = most_perfect_guess[@perfect_spot[0].to_i]
-        elsif max_perfect == 2
-          @current_guess = [rand_digit, rand_digit, rand_digit]
-          @current_guess[@perfect_spot[0].to_i] = most_perfect_guess[@perfect_spot[0].to_i]
-          @current_guess[@perfect_spot[1].to_i] = most_perfect_guess[@perfect_spot[1].to_i]
-        else
-          @current_guess = [rand_digit, rand_digit, rand_digit]
-        end
-
-        if ! @guess_array.include?(@current_guess.join(""))
-          break
-        end
+    if num_correct + num_misplaced == 0 && i > 1
+      @valid_guesses = @valid_guesses.delete(@guess_array[i-1])
+    elsif num_correct + num_misplaced == 3 && i > 1
+      return @guess_array[i] = guess_array[i-1].split("").shuffle.join("")
+    elsif i > 1
+      for guess_array[i-1].split("").each do |c|
+        @weight[c.to_i] += 2
       end
     end
-    @guess_array[i] = @current_guess.join("")
+
+    len = @valid_guesses.length
+    @guess_array[i] = @valid_guesses[ rand(len) ] +  
+                      @valid_guesses[ rand(len) ] +  
+                      @valid_guesses[ rand(len) ]
+  
+
   end
 
-  def next_try
-    array = ["12","20","01"]
-    array [(array.index(@perfect_spot) + 1) % 3]
-  end
-
-  def rand_digit
-    @valid_guesses[ rand( @valid_guesses.length ) ] 
-  end
 end
+
 
 class Score
   attr_reader :correct, :misplaced  
